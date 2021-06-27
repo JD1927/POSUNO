d922ecee264187321688aebdeae726d2f326cb56
Juan Aguirre <j1927aguirre@gmail.com>
2021-06-27 00:47:03 -0500

    Create basic Login request

---
+++ POSUNO/POSUNO.API/Models/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POSUNO.API.Models
+{
+    public class LoginResponse
+    {
+    }
+}
