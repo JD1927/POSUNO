6db22f509db8d9338738ae7f6e1195d0405c7046
Juan Aguirre <j1927aguirre@gmail.com>
2021-06-30 22:22:12 -0500

    Add Product and Customer list view

--- POSUNO/POSUNO/POSUNO.Shared/Helpers/APIService.cs
+++ POSUNO/POSUNO/POSUNO.Shared/Helpers/APIService.cs
@@ -16,6 +16,7 @@ namespace POSUNO.Helpers
             {
                 string request = JsonConvert.SerializeObject(loginModel);
                 StringContent content = new StringContent(request, Encoding.UTF8, "application/json");
+
                 HttpClientHandler handler = new HttpClientHandler()
                 {
                     ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
@@ -49,6 +50,47 @@ namespace POSUNO.Helpers
                 {
                     IsSuccess = false,
                     Message = ex.Message,
+                };
+            }
+        }
+
+        public static async Task<APIResponse> GetListAsync<T>(string controller)
+        {
+            try
+            {
+                HttpClientHandler handler = new HttpClientHandler()
+                {
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                };
+                string url = Settings.GetApiUrl();
+                HttpClient client = new HttpClient(handler)
+                {
+                    BaseAddress = new Uri(url)
+                };
+                HttpResponseMessage response = await client.GetAsync($"api/{controller}");
+                string result = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new APIResponse()
+                    {
+                        IsSuccess = false,
+                        Message = result,
+                    };
+                }
+
+                List<T> list = JsonConvert.DeserializeObject<List<T>>(result);
+                return new APIResponse()
+                {
+                    IsSuccess = true,
+                    Result = list,
+                };
+            }
+            catch (Exception ex)
+            {
+                return new APIResponse()
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
                 };
             }
         }
--- POSUNO/POSUNO/POSUNO.Shared/Pages/CustomersPage.xaml.cs
+++ POSUNO/POSUNO/POSUNO.Shared/Pages/CustomersPage.xaml.cs
@@ -1,7 +1,13 @@
-﻿using POSUNO.Models;
+﻿using POSUNO.Components;
+using POSUNO.Helpers;
+using POSUNO.Models;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -18,6 +24,37 @@ namespace POSUNO.Pages
         }
 
         public ObservableCollection<Customer> Customers { get; set; }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            GetCustomersAsync();
+        }
+
+        private async void GetCustomersAsync()
+        {
+            Loader loader = new Loader("Por favor espere...");
+            loader.Show();
+            APIResponse response = await APIService.GetListAsync<Customer>("customers");
+            loader.Close();
+            if (!response.IsSuccess)
+            {
+                MessageDialog dialog = new MessageDialog(response.Message, "Error");
+                await dialog.ShowAsync();
+                return;
+            }
+
+            List<Customer> customers = (List<Customer>)response.Result;
+            Customers = new ObservableCollection<Customer>(customers);
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            CustomersListView.ItemsSource = null;
+            CustomersListView.Items.Clear();
+            CustomersListView.ItemsSource = Customers;
+        }
 
         private void EditTapped(object sender, TappedRoutedEventArgs e)
         {
